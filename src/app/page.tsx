01de0387e9d17aeeb36de7d8ef28b9987faa6002
Ryan <ryan567.pr@gmail.com>
2025-02-04 10:27:56 -0300

    :bug: fix: corrigido a responsividade do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import { CardWrapper } from "@/components/auth/CardWrapper";
 
 export default async function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-background">
+    <main className="flex h-full flex-col items-center justify-center bg-background p-4">
       <CardWrapper
         headerLabel="Organize seu dia, conquiste seus objetivos!"
-        className="w-[500px]"
+        className="w-full md:w-[500px]"
         backButtonHref=""
       >
         <section className="space-y-6 text-center">
           <section>
             <LoginButon mode="modal" asChild>
-              <Button variant="default" size="lg">
+              <Button variant="default" size="lg" className="w-full md:w-auto">
                 ENTRAR
               </Button>
             </LoginButon>
